2342b37acdb83f5b9d287f6892801e37681ec3c2
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-17 13:07:27 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from . import __app,find_db_file,db
+from . import __app,find_db_file,db,request,render_template
 
 #可以設置app的名字
 app = _app()
@@ -11,6 +11,25 @@ db.init_app(app)
 def index():
   return "Hello Page"
 
-@app.route("/query/<statement>",methods=["GET","POST"])
-def do_event(statement):
-  if 
+@app.route("/create_table", methods=["POST"])
+def create_model():
+    table_name = request.form.get("table_name")
+    fields = request.form.get("fields").split(",")
+
+    # 動態創建模型
+    attributes = {'__tablename__': table_name.lower()}
+    for field in fields:
+        name, field_type = field.split(":")
+        if field_type == "String":
+            attributes[name] = db.Column(db.String(80))
+        elif field_type == "Integer":
+            attributes[name] = db.Column(db.Integer)
+        elif field_type == "Float":
+            attributes[name] = db.Column(db.Float)
+        elif field_type == "Boolean"
+            attributes[name] = db.Column(db.Boolean)
+
+    # 創建新的模型類
+    new_model = type(table_name, (BaseModel,), attributes)
+    db.create_all()  # 創建資料表
+    return f"資料表 '{table_name}' 已經建立這些欄位: {', '.join(fields)}"

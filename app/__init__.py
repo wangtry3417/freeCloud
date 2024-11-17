62653e38f1fd7694d57a29fa8843b843abb466da
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-17 12:27:24 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-def __app(appName=None):
-  if app != None:
+def _app(appName=None):
+  if appName != None:
     return Flask(appName)
   else:
     return Flask(__name__)
 
 def find_db_file(filename) -> str:
-  return "sqlite://"+filename
+  return "sqlite:///"+filename
 
 __all__ = ["__app","find_db_file"]

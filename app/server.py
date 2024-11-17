c1ea7bf0feaa3463021b3b72056f638b09fcb27b
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-17 12:35:31 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,10 +1,9 @@
-from . import __app,find_db_file
+from . import __app,find_db_file,db
 
 #可以設置app的名字
 app = _app()
 #輸入sqlite協定的database文件
 dbFile = find_db_file("test.db")
-db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = dbFile
 db.init_app(app)
 

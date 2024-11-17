897221186be9a5a2b9c6266d8eea9b8d1f27d599
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-17 14:26:28 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 def _app(appName=None):
   if appName != None:
-    return Flask(appName,template_folder=".pages")
+    return Flask(appName,template_folder="pages")
   else:
-    return Flask(__name__,template_folder=".pages")
+    return Flask(__name__,template_folder="pages")
 
 def find_db_file(filename) -> str:
   return "sqlite:///"+filename

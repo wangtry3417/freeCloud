f5a16a75bd4852ce0114ed3a030a6692f6b23e6c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-17 19:21:09 +0800

    Update start.py

--- start.py
+++ start.py
@@ -7,3 +7,10 @@ if not os.path.exists("main.db"):
 if __name__ == "__main__":
   from app.server import run_app
   run_app()
+
+"""
+如果不想web server，也可以datatable
+"""
+"""
+from app.server.models import baseModel
+"""

e9e8d50543c630bf28d9132441b3bc0052d405e0
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-17 12:27:54 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -2,7 +2,7 @@ from . import __app,find_db_file
 from flask_sqlalchemy import SQLAlchemy
 
 #可以設置app的名字
-app = __app()
+app = _app()
 #輸入sqlite協定的database文件
 dbFile = find_db_file("test.db")
 db = SQLAlchemy()

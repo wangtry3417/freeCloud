1021364505d2e26d783d54b8ea8003e978ae8867
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-22 14:25:07 +0800

    Update __init__.py

--- src/trydb/models/__init__.py
+++ src/trydb/models/__init__.py
@@ -1,7 +1,15 @@
-from .. import declarative_base,String,Integer,Boolean,Column,Model
+from datetime import datetime
+from sqlalchemy import Column, String, Integer, Boolean, DateTime
+from .. import declarative_base
+
+Model = declarative_base()
 
 class BaseModel(Model):
-  id = Column(Integer,primary_key=True)
-  #Add fields
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True)
+  
+    def __repr__(self):
+        return f"<BaseModel(id={self.id})>"
 
 __all__ = ["BaseModel"]

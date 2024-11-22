fa8f43148aa52e1c36e92e9e169d20179e5c9362
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-22 13:08:35 +0800

    Update __init__.py

--- src/trydb/__init__.py
+++ src/trydb/__init__.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, create_engine, String, Integer, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
+def DataBase(url:str,useSession=True : bool):
+  if useSession:
+    engine = create_engine(url)
+    return sessionmaker(bind=engine)
+  else:
+    return create_engine(url)
+
+__all__ = ["DataBase","String","Integer","Boolean","declarative_base"]

fd684d6d3394263e4d3f39a163a2c6d44aa38677
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-22 17:35:03 +0800

    Update __init__.py

--- src/trydb/complier/__init__.py
+++ src/trydb/complier/__init__.py
@@ -1,12 +1,12 @@
  # compiler/__init__.py
-from .. import DataBase  # 假設 db_instance 在 init.py 中定義
+# from .. import DataBase  # 假設 db_instance 在 init.py 中定義
 from .insert import handle_insert
 from .select import handle_select
 from .create_table import handle_create_table
 
 dynamic_models = {}
 
-def tryDB(tryDB_input):
+def tryDB(tryDB_input,DataBase):
     if tryDB_input:
         try:
             if "Insert" in tryDB_input:

ec6d7900fc172bec6d11b548565c045e2287ea65
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-22 17:14:14 +0800

    Update __init__.py

--- src/trydb/complier/__init__.py
+++ src/trydb/complier/__init__.py
@@ -10,11 +10,11 @@ def tryDB(tryDB_input):
     if tryDB_input:
         try:
             if "Insert" in tryDB_input:
-                return handle_insert(tryDB_input, dynamic_models)
+                return handle_insert(tryDB_input, DataBase)
             elif "using" in tryDB_input:
-                return handle_select(tryDB_input, dynamic_models)
+                return handle_select(tryDB_input, DataBase)
             elif "create table" in tryDB_input:
-                return handle_create_table(tryDB_input, dynamic_models)
+                return handle_create_table(tryDB_input, DataBase)
             else:
                 return {"error": "不支援的指令格式。"}
         except Exception as e:
